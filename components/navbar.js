e56c87cbe6b1ad557188542599671ecd6ea50744
Vendll <utto.vendel@icloud.com>
2022-03-18 20:53:05 +0100

    Products page and components applied

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,313 @@
+import Image from 'next/image'
+import { Fragment, useState } from 'react'
+import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
+import { MenuIcon, SearchIcon, ShoppingBagIcon, UserIcon, XIcon } from '@heroicons/react/outline'
+import logoPic from '../public/BK.svg'
+
+
+const navigation = {
+  categories: [
+    {
+      id: 'products',
+      name: 'Termékek',
+      sections: [
+        [
+          {
+            id: 'shoes',
+            name: 'Shoes & Accessories',
+            items: [
+              {  },
+              { name: 'Virslik', href: '#' },
+              { name: 'Babok', href: '#' },
+              { name: 'Szószok', href: '#' },
+              { name: 'Tojások', href: '#' },
+              { name: 'Sonkák', href: '#' },
+              { name: 'Töltelékek', href: '#' },
+            ],
+          },
+        ],
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Rólunk', href: '/products' },
+    { name: 'Képek', href: '#' },
+    { name: 'Blog', href: '#' },
+    { name: 'Kapcsolat', href: '#' },
+  ],
+}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
     return (
-      <>
-        <main>Hi</main>
-      </>
+      <div className="bg-white fixed top-0 z-30 w-full">
+      {/* Mobile menu */}
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div className="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+              <div className="px-4 pt-5 pb-2 flex">
+                <button
+                  type="button"
+                  className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                  onClick={() => setOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+
+              {/* Links */}
+              <Tab.Group as="div" className="mt-2">
+                <div className="border-b border-gray-200">
+                  <Tab.List className="-mb-px flex px-4 space-x-8">
+                    {navigation.categories.map((category) => (
+                      <Tab
+                        key={category.name}
+                        className={({ selected }) =>
+                          classNames(
+                            selected ? 'text-indigo-600 border-indigo-600' : 'text-gray-900 border-transparent',
+                            'flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium'
+                          )
+                        }
+                      >
+                        {category.name}
+                      </Tab>
+                    ))}
+                  </Tab.List>
+                </div>
+                <Tab.Panels as={Fragment}>
+                  {navigation.categories.map((category) => (
+                    <Tab.Panel key={category.name} className="pt-10 pb-8 px-4 space-y-10">
+                      {category.sections.map((column, columnIdx) => (
+                        <div key={columnIdx} className="space-y-10">
+                          {column.map((section) => (
+                            <div key={section.name}>
+                              <p
+                                id={`${category.id}-${section.id}-heading-mobile`}
+                                className="font-medium text-gray-900"
+                              >
+                                {section.name}
+                              </p>
+                              <ul
+                                role="list"
+                                aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
+                                className="mt-6 flex flex-col space-y-6"
+                              >
+                                {section.items.map((item) => (
+                                  <li key={item.name} className="flow-root">
+                                    <a href={item.href} className="-m-2 p-2 block text-gray-500">
+                                      {item.name}
+                                    </a>
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                          ))}
+                        </div>
+                      ))}
+                    </Tab.Panel>
+                  ))}
+                </Tab.Panels>
+              </Tab.Group>
+
+              <div className="border-t border-gray-200 py-6 px-4 space-y-6">
+                {navigation.pages.map((page) => (
+                  <div key={page.name} className="flow-root">
+                    <a href={page.href} className="-m-2 p-2 block font-medium text-gray-900">
+                      {page.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+
+            </div>
+          </Transition.Child>
+        </Dialog>
+      </Transition.Root>
+
+            {/* desktop menu */}
+      <header className="relative bg-white">
+        <nav aria-label="Top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="border-b border-gray-200">
+            <div className="h-24 flex items-center justify-between">
+              <div className="flex-1 flex items-center lg:hidden">
+                <button
+                  type="button"
+                  className="-ml-2 bg-white p-2 rounded-md text-gray-400"
+                  onClick={() => setOpen(true)}
+                >
+                  <span className="sr-only">Open menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                <a href="#" className="ml-2 p-2 text-gray-400 hover:text-gray-500">
+                  <span className="sr-only">Search</span>
+                  <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                </a>
+              </div>
+
+              {/* Flyout menus */}
+              <Popover.Group className="hidden lg:flex-1 lg:block lg:self-stretch">
+                <div className="h-full flex space-x-8">
+                  {navigation.categories.map((category) => (
+                    <Popover key={category.name} className="flex">
+                      {({ open }) => (
+                        <>
+                          <div className="relative flex">
+                            <Popover.Button
+                              className={classNames(
+                                open ? 'text-bobred' : 'text-bobgray hover:text-bobblue',
+                                'relative z-10 flex items-center justify-center transition-colors ease-out duration-200 text-sm font-medium hover:border-b-2 hover:border-bobred'
+                              )}
+                            >
+                              {category.name}
+                              <span
+                                className={classNames(
+                                  open ? 'bg-indigo-600' : '',
+                                  'absolute bottom-0 inset-x-0 h-0.5 transition-colors ease-out duration-200 sm:mt-5 sm:transform sm:translate-y-px'
+                                )}
+                                aria-hidden="true"
+                              />
+                            </Popover.Button>
+                          </div>
+
+                          <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Popover.Panel className="absolute top-full z-20 inset-x-0">
+                              {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+                              <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
+
+                              <div className="relative bg-white">
+                                <div className="max-w-7xl mx-auto px-8">
+                                  <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
+                                    <div className="grid grid-cols-3 gap-y-10 gap-x-8 text-sm text-gray-500">
+                                      {category.sections.map((column, columnIdx) => (
+                                        <div key={columnIdx} className="space-y-10">
+                                          {column.map((section) => (
+                                            <div key={section.name}>
+                                              <p
+                                                id={`${category.id}-${section.id}-heading`}
+                                                className="font-medium text-gray-900"
+                                              >
+                                                {section.name}
+                                              </p>
+                                              <ul
+                                                role="list"
+                                                aria-labelledby={`${category.id}-${section.id}-heading`}
+                                                className="mt-4 space-y-4"
+                                              >
+                                                {section.items.map((item) => (
+                                                  <li key={item.name} className="flex">
+                                                    <a href={item.href} className="hover:text-gray-800">
+                                                      {item.name}
+                                                    </a>
+                                                  </li>
+                                                ))}
+                                              </ul>
+                                            </div>
+                                          ))}
+                                        </div>
+                                      ))}
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </Popover.Panel>
+                          </Transition>
+                        </>
+                      )}
+                    </Popover>
+                  ))}
+
+                  {navigation.pages.map((page) => (
+                    <a
+                      key={page.name}
+                      href={page.href}
+                      className="flex items-center text-sm font-medium text-bobgray hover:text-bobblue hover:border-b-2 hover:border-bobred"
+                    >
+                      {page.name}
+                    </a>
+                  ))}
+                </div>
+              </Popover.Group>
+
+              {/* Logo */}
+              <a href="/" className="flex">
+                <span className="sr-only">Workflow</span>
+                <Image
+                  className="h-16 w-auto"
+                  src={logoPic}
+                  layout="fixed"
+                  height={100}
+                  width={50}
+                  alt=""
+                />
+              </a>
+
+              <div className="flex-1 flex items-center justify-end">
+
+                {/* Search */}
+                <a href="#" className="hidden ml-6 p-2 text-gray-400 hover:text-bobgray lg:block">
+                  <span className="sr-only">Search</span>
+                  <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                </a>
+
+                {/* Account */}
+                <a href="#" className="p-2 text-gray-400 hover:text-bobgray lg:ml-4">
+                  <span className="sr-only">Account</span>
+                  <UserIcon className="w-6 h-6" aria-hidden="true" />
+                </a>
+
+                {/* Cart */}
+                <div className="ml-4 flow-root lg:ml-6">
+                  <a href="#" className="group -m-2 p-2 flex items-center">
+                    <ShoppingBagIcon
+                      className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-bobblue"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                    <span className="sr-only">items in cart, view bag</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+
     )
   }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,3 +1,96 @@
-export default function Products() {
-    return <div className="flex content-center justify-center">Products</div>
+import Image from "next/image"
+import Layout from "../../components/layout"
+
+const products = [
+    {
+      id: 1,
+      name: 'Focus Paper Refill',
+      href: '#',
+      price: '$13',
+      description: '3 sizes available',
+      imageSrc: '/image2/image00005.jpeg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 2,
+      name: 'Focus Card Holder',
+      href: '#',
+      price: '$64',
+      description: 'Walnut',
+      imageSrc: '/image2/image00003.jpeg',
+      imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+    },
+    {
+      id: 3,
+      name: 'Focus Carry Case',
+      href: '#',
+      price: '$32',
+      description: 'Heather Gray',
+      imageSrc: '/image2/image00004.jpeg',
+      imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
+    },
+    {
+      id: 1,
+      name: 'Focus Paper Refill',
+      href: '#',
+      price: '$13',
+      description: '3 sizes available',
+      imageSrc: '/image2/image00005.jpeg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 2,
+      name: 'Focus Card Holder',
+      href: '#',
+      price: '$64',
+      description: 'Walnut',
+      imageSrc: '/image2/image00003.jpeg',
+      imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+    },
+    {
+      id: 3,
+      name: 'Focus Carry Case',
+      href: '#',
+      price: '$32',
+      description: 'Heather Gray',
+      imageSrc: '/image2/image00004.jpeg',
+      imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
+    },
+  ]
+  
+  export default function Products() {
+    return (
+        <Layout>
+            <div className="bg-white">
+              <div className="max-w-2xl mx-auto pt-16 mt-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h2 id="products-heading" className="sr-only">
+                  Összes Termék
+                </h2>
+
+                <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+                  {products.map((product) => (
+                    <a key={product.id} href={product.href} className="group">
+                      <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
+                        <Image
+                          src={product.imageSrc}
+                          alt={product.imageAlt}
+                          layout="responsive"
+                          width="65%"
+                          height="80%"
+                          objectFit='cover'
+                          className="w-full h-full object-center object-cover group-hover:opacity-75"
+                        />
+                      </div>
+                      <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
+                        <h3>{product.name}</h3>
+                        <p>{product.price}</p>
+                      </div>
+                      <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+        </Layout>
+    )
 }
