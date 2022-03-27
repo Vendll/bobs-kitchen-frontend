6b96530d693cd45e779c1b7384a8949a2eb2cf64
Vendll <utto.vendel@icloud.com>
2022-03-27 12:46:13 +0200

    ALL links fixed with Link component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Blog() {
   const posts = [
@@ -135,30 +136,34 @@ export default function Blog() {
                 <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                   <div className="flex-1">
                     <p className="text-sm font-medium text-indigo-600">
-                      <a href={post.category.href} className="hover:underline">
-                        {post.category.name}
-                      </a>
+                      <Link href={post.category.href}>
+                        <a className="hover:underline">{post.category.name}</a>
+                      </Link>
                     </p>
-                    <a href={post.href} className="block mt-2">
-                      <p className="text-xl font-semibold text-gray-900">
-                        {post.title}
-                      </p>
-                      <p className="mt-3 text-base text-gray-500">
-                        {post.description}
-                      </p>
-                    </a>
+                    <Link href={post.href}>
+                      <a className="block mt-2">
+                        <p className="text-xl font-semibold text-gray-900">
+                          {post.title}
+                        </p>
+                        <p className="mt-3 text-base text-gray-500">
+                          {post.description}
+                        </p>
+                      </a>
+                    </Link>
                   </div>
                   <div className="mt-6 flex items-center">
                     <div className="flex-shrink-0 ">
-                      <a href={post.author.href} className="relative h-10">
-                        <span className="sr-only">{post.author.name}</span>
-                      </a>
+                      <Link href={post.author.href}>
+                        <a className="relative h-10">
+                          <span className="sr-only">{post.author.name}</span>
+                        </a>
+                      </Link>
                     </div>
                     <div className="ml-3">
                       <p className="text-sm font-medium text-gray-900">
-                        <a href={post.author.href} className="hover:underline">
-                          {post.author.name}
-                        </a>
+                        <Link href={post.author.href}>
+                          <a className="hover:underline">{post.author.name}</a>
+                        </Link>
                       </p>
                       <div className="flex space-x-1 text-sm text-gray-500">
                         <time dateTime={post.datetime}>{post.date}</time>
