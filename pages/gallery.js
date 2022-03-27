4acd9cafb54a4ab1ff586018f6e69f7fde7bb726
Vendll <utto.vendel@icloud.com>
2022-03-27 19:39:44 +0200

    Gallery fixed image import and working lightbox

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,4 +1,5 @@
 import FsLightbox from "fslightbox-react";
+import Image from "next/image";
 
 import Layout from "../components/Layout";
 
@@ -11,23 +12,19 @@ import contactPic5 from "../public/image1/image00001.jpeg";
 import contactPic6 from "../public/image1/image00002.jpeg";
 import contactPic7 from "../public/image1/image00003.jpeg";
 
-let productsImages = [
-  { photo: contactPic, key: 1 },
-  { photo: contactPic2, key: 2 },
-  { photo: contactPic3, key: 3 },
-  { photo: contactPic4, key: 4 },
+let productImages = [
+  "image2/image00005.jpeg",
+  "image1/image00006.jpeg",
+  "image1/image00007.jpeg",
+  "image1/image00008.jpeg",
+
+  /* { contactPic },
+  { contactPic2 },
+  { contactPic3 },
+  { contactPic4 }, */
 ];
+
 export default function MyModal() {
-  let [isOpen, setIsOpen] = useState(false);
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
   const [lightboxController, setLightboxController] = useState({
     toggler: false,
     slide: 1,
@@ -39,17 +36,16 @@ export default function MyModal() {
       slide: number,
     });
   }
-
   return (
     <Layout>
-      <>
-        {/*         <div className="mx-36 h-[50vh] mt-36">
-          <div className="flex-col sm:grid mx-3 sm:mx-0 sm:grid-cols-2 md:grid-cols-3  gap-4 my-24">
-            <div className="KEPMODAL border-2 border-bobred ">
-              <div className="border-2 border-bobblue inset-0 flex items-center justify-center">
+      <div className="px-4 md:px-32 pt-32 pb-8 bg-gray-300">
+        <div className="flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {productImages.map((item, index) => (
+            <div className="KEPMODAL border-2 border-bobred bg-gray-200 rounded-md my-4 sm:my-0">
+              <div className="border-2 border-bobblue rounded-md inset-0 flex items-center justify-center">
                 <div
-                  onClick={() => openLightboxOnSlide(1)}
-                  className="p-4 w-full h-96 hover:opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                  onClick={() => openLightboxOnSlide(index + 1)}
+                  className="px-4 pt-4 w-full h-[22rem] hover:opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                 >
                   <div className="block relative h-72 border-2 border-bobred">
                     <Image
@@ -59,16 +55,16 @@ export default function MyModal() {
                       key="1342"
                     />
                   </div>
-                  <div className="text-center mt-2">
+                  <div className="text-center mt-2 break-all">
                     Niggaaaaaaaaasaaasasasasasasasas
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-        <div className="mt-36 h-96">
-          <button onClick={() => openLightboxOnSlide(1)}>
+      </div>
+      {/* <button onClick={() => openLightboxOnSlide(1)}>
             Open lightbox on first slide.
           </button>
           <br></br>
@@ -82,15 +78,13 @@ export default function MyModal() {
           <br></br>
           <button onClick={() => openLightboxOnSlide(4)}>
             Open lightbox on 4 slide.
-          </button>
+          </button> */}
 
-          <FsLightbox
-            toggler={lightboxController.toggler}
-            sources={productsImages}
-            slide={lightboxController.slide}
-          />
-        </div> */}
-      </>
+      <FsLightbox
+        toggler={lightboxController.toggler}
+        sources={productImages}
+        slide={lightboxController.slide}
+      />
     </Layout>
   );
 }
